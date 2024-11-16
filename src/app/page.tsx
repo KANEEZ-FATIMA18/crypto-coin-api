5d5086da92fe8a19fc1352a8fa4ef4bc58397dac
KANEEZ-FATIMA18 <k.fatima182005@gmail.com>
2024-11-16 19:33:48 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
+import Link from "next/link"
 export default function Home() {
   return (
-    <div>
-    Hello,  Plzz visit coin.market page
-
+    <div className="bg-cyan-900 min-h-screen flex justify-center items-center">
+    <Link href={'/coin.market'} className="shadow-xl"><h1 className="text-5xl text-center font-[poppins] text-white">Hello,<br /> Plzz visit coin.market page
+    </h1></Link>  
+    
     </div>
   )}
